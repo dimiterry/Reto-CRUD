dffff2795b0c873063326d1b396f61d70c0eee8f
Jorge Elías Osorno Ruiz <jorge.osorno@sofka.local>
2025-02-01 20:16:26 -0500

    borrar tareas completado

--- client/src/context/TasksContext.jsx
+++ client/src/context/TasksContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from "react";
-import { createTaskRequest, getTasksRequest } from '../api/tasks'
+import { createTaskRequest, getTasksRequest, deleteTaskRequest } from '../api/tasks'
 
 const TaskContext = createContext();
 
@@ -23,12 +23,23 @@ export function TaskProvider({ children }) {
         } catch (error) {
             console.error(error);
         }
-    }
+    };
 
     const createTask = async (task) => {
         const res = await createTaskRequest(task)
         console.log(res)
-    }
+    };
+
+    const deleteTask = async (id) => {
+        try {
+            const res = await deleteTaskRequest(id);
+            if (res.status === 204) setTasks(tasks.filter(task => task._id !== id));
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    };
 
     return (
         <TaskContext.Provider
@@ -36,6 +47,7 @@ export function TaskProvider({ children }) {
                 tasks,
                 createTask,
                 getTasks,
+                deleteTask,
             }}
         >
             {children}
